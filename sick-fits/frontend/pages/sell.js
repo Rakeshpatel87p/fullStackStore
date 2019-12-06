2fd046d95b5904207fc8c9f3a92d40de654e0c72
rakeshpatel87p <rakesh.patel@accesscfa.com>
2019-12-06 18:43:23 -0500

    new pages setup, basic routing with next

---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+
+const Sell = props => {
+    return (
+        <div>
+            <p>Hello World!</p>
+            <Link href="/">
+                <a>Home</a>
+            </Link>
+        </div>
+    )
+}
+
+export default Sell
