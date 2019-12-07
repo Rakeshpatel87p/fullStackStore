6e4e3410139209830c15d263af658caea4f2c605
rakeshpatel87p <rakesh.patel@accesscfa.com>
2019-12-06 19:56:43 -0500

    rmv link

--- sick-fits/frontend/pages/sell.js
+++ sick-fits/frontend/pages/sell.js
@@ -4,9 +4,6 @@ const Sell = props => {
     return (
         <div>
             <p>Hello World!</p>
-            <Link href="/">
-                <a>Home</a>
-            </Link>
         </div>
     )
 }
