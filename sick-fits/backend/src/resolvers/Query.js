0b93294d19b3dcc7ad2e60f2a3099b8616dd88ae
rakeshpatel87p <rakesh.patel@accesscfa.com>
2019-12-13 10:31:39 -0500

    using forwardTo

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,9 +1,12 @@
+const {forwardTo} = require('prisma-binding')
+
 const Query = {
-    async items(parent, args, ctx, info) {
-        const items = await ctx.db.query.items()
-        console.log(items)
-        return items
-    }
+    items: forwardTo('db')
+    // async items(parent, args, ctx, info) {
+    //     const items = await ctx.db.query.items()
+    //     console.log(items)
+    //     return items
+    // }
 };
 
 module.exports = Query;
