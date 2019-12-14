92c20669aa5fd359910d5732c4ff3294180d46ea
rakeshpatel87p <rakesh.patel@accesscfa.com>
2019-12-14 14:35:15 -0500

    make request for items!

--- sick-fits/frontend/components/Items.js
+++ sick-fits/frontend/components/Items.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
 import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
+
+const ALL_ITEMS_QUERY = gql`
+    query ALL_ITEMS_QUERY {
+        items {
+            id
+            title
+            price
+            description
+            image
+            largeImage
+        }
+    }
+`
 
 export default class Items extends Component {
     render() {
         return (
             <div>
                 <p>Items</p>
+                <Query query={ALL_ITEMS_QUERY}>
+                    {({data, error, loading}) => {
+                        console.log(data)
+                        return (
+                            <div>
+                                <p>We have items</p>
+                            </div>
+                        )
+                    }}
+                </Query>
             </div>
         )
     }
