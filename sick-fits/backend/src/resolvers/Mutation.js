816bcaa8803a6926b8dd93f0c724268945ebf28c
rakeshpatel87p <rakesh.patel@accesscfa.com>
2019-12-18 18:33:41 -0500

    BE endpoints setup

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,13 +1,41 @@
+//Resolve request on database
+
 const Mutations = {
-    async createItem(parent, args, ctx, info) {
-        const item = await ctx.db.mutation.createItem({
-            data: {...args}
-        }, info)
+  async createItem(parent, args, ctx, info) {
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: { ...args }
+      },
+      info
+    );
 
-        console.log(item)
+    console.log(item);
 
-        return item
-    }
+    return item;
+  },
+
+  updateItem(parent, args, ctx, info) {
+    // first take a copy of the updates
+    const updates = { ...args };
+    // remove the ID from the updates
+    delete updates.id;
+
+    return ctx.db.mutations.updateItem(
+      {
+        data: updates,
+        where: {
+          id: args.id
+        }
+      },
+      info
+    );
+  },
+
+  async deleteItem(parent, args, ctx, info) {
+    const where = { id: args.id };
+    const item = ctx.db.query.item({ where }, `{id title}`);
+    return ctx.db.mutation.deleteItem({ where }, info);
+  }
 };
 
 module.exports = Mutations;
