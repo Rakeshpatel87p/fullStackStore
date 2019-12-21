e5ad1dd622b9126f8c3472666ea7f79efdb224e9
rakeshpatel87p <rakesh.patel@accesscfa.com>
2019-12-21 16:09:37 -0500

    new pagination component integrated

--- sick-fits/frontend/components/Items.js
+++ sick-fits/frontend/components/Items.js
@@ -1,8 +1,9 @@
-import React, { Component } from 'react';
-import { Query } from 'react-apollo';
-import gql from 'graphql-tag';
-import styled from 'styled-components';
-import Item from './Item';
+import React, { Component } from "react";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+import styled from "styled-components";
+import Item from "./Item";
+import Pagination from "./Pagination";
 
 const ALL_ITEMS_QUERY = gql`
   query ALL_ITEMS_QUERY {
@@ -33,19 +34,27 @@ class Items extends Component {
   render() {
     return (
       <Center>
-        <Query query={ALL_ITEMS_QUERY}>
-          {({ data, error, loading }) => {
-            if (loading) return <p>Loading...</p>;
-            if (error) return <p>Error: {error.message}</p>;
-            return (
-              <ItemsList>{data.items.map(item => <Item item={item} key={item.id} />)}</ItemsList>
-            );
-          }}
-        </Query>
+        <Pagination page={this.props.page}>
+          <Query query={ALL_ITEMS_QUERY}>
+            {({ data, error, loading }) => {
+              if (loading) return <p>Loading...</p>;
+              if (error) return <p>Error: {error.message}</p>;
+              return (
+                <div>
+                  <ItemsList>
+                    {data.items.map(item => (
+                      <Item item={item} key={item.id} />
+                    ))}
+                  </ItemsList>
+                </div>
+              );
+            }}
+          </Query>
+        </Pagination>
       </Center>
     );
   }
 }
 
 export default Items;
-export { ALL_ITEMS_QUERY }  
+export { ALL_ITEMS_QUERY };
