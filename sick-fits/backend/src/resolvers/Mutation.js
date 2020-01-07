05e845aeda04c7f21a1048d4d326e58b3306895d
rakeshpatel87p <rakesh.patel@accesscfa.com>
2020-01-07 17:22:37 -0500

    addToCart mutation added

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -37,6 +37,35 @@ const Mutations = {
     const item = await ctx.db.query.item({ where }, `{id title}`);
     // 3. Delete it!
     return ctx.db.mutation.deleteItem({ where }, info);
+  },
+  async addToCart(parent, args, ctx, info) {
+    //Check if item is already in the cart and increment by 1 if it is
+    const [existingCartItem] = await ctx.db.query.cartItems({
+      where: {
+        item: { id: args.id }
+      }
+    });
+    if (existingCartItem) {
+      console.log("This item is already in their cart");
+      return ctx.db.mutation.updateCartItem(
+        {
+          where: { id: existingCartItem.id },
+          data: { quantity: existingCartItem.quantity + 1 }
+        },
+        info
+      );
+    }
+    //if its not, create fresh cartItem for visitor
+    return ctx.db.mutation.createCartItem(
+      {
+        data: {
+          item: {
+            connect: { id: args.id }
+          }
+        }
+      },
+      info
+    );
   }
 };
 
