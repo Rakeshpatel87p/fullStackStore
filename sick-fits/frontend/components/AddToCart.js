96c45698c56a022d41e1aa58b2af63bbf580fe2a
rakeshpatel87p <rakesh.patel@accesscfa.com>
2020-01-09 19:18:39 -0500

    adding new local state mutation

--- sick-fits/frontend/components/AddToCart.js
+++ sick-fits/frontend/components/AddToCart.js
@@ -2,10 +2,29 @@ import React, { Component } from "react";
 import { Mutation } from "react-apollo";
 import gql from "graphql-tag";
 
+const ADD_TO_CART_MUTATION = gql`
+  mutation ADD_TO_CART_MUTATION($id: String!) {
+    addToCart(id: $id) @client {
+      id
+    }
+  }
+`;
+
+const TOGGLE_CART_MUTATION = gql`
+  mutation {
+    toggleCart @client
+  }
+`;
+
 class AddToCart extends Component {
   render() {
     const { id } = this.props;
-    return <button>Add To Cart</button>;
+    return (
+      //<button onClick>Add To Cart</button>
+      <Mutation mutation={ADD_TO_CART_MUTATION} variables={{ id }}>
+        {addToCart => <button onClick={addToCart}>Add To Cart</button>}
+      </Mutation>
+    );
   }
 }
 
