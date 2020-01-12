e44563a8d9fe546b5e83b985b38bcabcc057c3c6
rakeshpatel87p <rakesh.patel@accesscfa.com>
2020-01-12 11:41:45 -0500

    clean up react render props with adopt

--- sick-fits/frontend/components/Cart.js
+++ sick-fits/frontend/components/Cart.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Query, Mutation } from "react-apollo";
 import gql from "graphql-tag";
+import { adopt } from "react-adopt";
 import CartStyles from "./styles/CartStyles";
 import Supreme from "./styles/Supreme";
 import CloseButton from "./styles/CloseButton";
@@ -18,29 +19,34 @@ const TOGGLE_CART_MUTATION = gql`
   }
 `;
 
-const Cart = () => (
-  <Mutation mutation={TOGGLE_CART_MUTATION}>
-    {toggleCart => (
-      <Query query={LOCAL_STATE_QUERY}>
-        {({ data }) => (
-          <CartStyles open={data.cartOpen}>
-            <header>
-              <CloseButton onClick={toggleCart} title="close">
-                &times;
-              </CloseButton>
-              <Supreme>Your Cart</Supreme>
-              <p>You Have __ Items in your cart.</p>
-            </header>
+const Composed = adopt({
+  cartOpenQuery: ({ render }) => (
+    <Query query={LOCAL_STATE_QUERY}>{render}</Query>
+  ),
+  toggleCartMutation: ({ render }) => (
+    <Mutation mutation={TOGGLE_CART_MUTATION}>{render}</Mutation>
+  )
+});
 
-            <footer>
-              <p>$10.10</p>
-              <SickButton>Checkout</SickButton>
-            </footer>
-          </CartStyles>
-        )}
-      </Query>
+const Cart = () => (
+  <Composed>
+    {({ cartOpenQuery, toggleCartMutation }) => (
+      <CartStyles open={cartOpenQuery.data.cartOpen}>
+        <header>
+          <CloseButton onClick={toggleCartMutation} title="close">
+            &times;
+          </CloseButton>
+          <Supreme>Your Cart</Supreme>
+          <p>You Have __ Items in your cart.</p>
+        </header>
+
+        <footer>
+          <p>$10.10</p>
+          <SickButton>Checkout</SickButton>
+        </footer>
+      </CartStyles>
     )}
-  </Mutation>
+  </Composed>
 );
 
 export default Cart;
