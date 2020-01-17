c2fa192e48ebb04754e028d802df1934f72164a6
rakeshpatel87p <rakesh.patel@accesscfa.com>
2020-01-16 23:53:26 -0500

    display more info for cart

--- sick-fits/frontend/components/Cart.js
+++ sick-fits/frontend/components/Cart.js
@@ -13,6 +13,12 @@ const LOCAL_STATE_QUERY = gql`
   }
 `;
 
+const CART_ITEMS_QUERY = gql`
+  query {
+    cartItems @client
+  }
+`;
+
 const TOGGLE_CART_MUTATION = gql`
   mutation {
     toggleCart @client
@@ -23,6 +29,9 @@ const Composed = adopt({
   cartOpenQuery: ({ render }) => (
     <Query query={LOCAL_STATE_QUERY}>{render}</Query>
   ),
+  cartItemsQuery: ({ render }) => (
+    <Query query={CART_ITEMS_QUERY}>{render}</Query>
+  ),
   toggleCartMutation: ({ render }) => (
     <Mutation mutation={TOGGLE_CART_MUTATION}>{render}</Mutation>
   )
@@ -30,22 +39,38 @@ const Composed = adopt({
 
 const Cart = () => (
   <Composed>
-    {({ cartOpenQuery, toggleCartMutation }) => (
-      <CartStyles open={cartOpenQuery.data.cartOpen}>
-        <header>
-          <CloseButton onClick={toggleCartMutation} title="close">
-            &times;
-          </CloseButton>
-          <Supreme>Your Cart</Supreme>
-          <p>You Have __ Items in your cart.</p>
-        </header>
-
-        <footer>
-          <p>$10.10</p>
-          <SickButton>Checkout</SickButton>
-        </footer>
-      </CartStyles>
-    )}
+    {({ cartOpenQuery, cartItemsQuery, toggleCartMutation }) => {
+      const cartItems = cartItemsQuery.data.cartItems;
+      if (cartItems.length <= 0) return null;
+      return (
+        <CartStyles open={cartOpenQuery.data.cartOpen}>
+          <header>
+            <CloseButton onClick={toggleCartMutation} title="close">
+              &times;
+            </CloseButton>
+            <Supreme>Your Cart</Supreme>
+            <p>
+              You Have {cartItems.length} Item
+              {cartItems.length > 1 ? "s" : null} in your cart.
+            </p>
+          </header>
+          <ul>
+            {cartItemsQuery.data.cartItems.map(item => (
+              <li>
+                <img width="100px" src={item.image} alt={item.title} />
+                <p>{item.title}</p>
+                <p>{item.price}</p>
+                <p>{item.quantity}</p>
+              </li>
+            ))}
+          </ul>
+          <footer>
+            <p>$10.10</p>
+            <SickButton>Checkout</SickButton>
+          </footer>
+        </CartStyles>
+      );
+    }}
   </Composed>
 );
 
