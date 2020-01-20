e500f726304099b6dc49c1ec76988c51231131b8
rakeshpatel87p <rakesh.patel@accesscfa.com>
2020-01-19 22:58:45 -0500

    cartCounter added

---
+++ sick-fits/frontend/components/CartCounter.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+import { CART_ITEMS_QUERY } from "./Cart";
+import styled from "styled-components";
+
+const CartCounterStyle = styled.p`
+  color: ${props => props.theme.red};
+  position: absolute;
+  right: 0;
+  top: -5px;
+`;
+
+const CartCounter = () => (
+  <Query query={CART_ITEMS_QUERY}>
+    {data => {
+      const { cartItems } = data.data;
+      return cartItems.length <= 0 ? null : (
+        <CartCounterStyle>
+          {cartItems.reduce(
+            (tally, currentVal) => tally + currentVal.quantity,
+            0
+          )}
+        </CartCounterStyle>
+      );
+    }}
+  </Query>
+);
+
+export default CartCounter;
