16cf2f5c8a5a44b09b26364cb55126d2eec39195
rakeshpatel87p <rakesh.patel@accesscfa.com>
2020-01-20 09:27:17 -0500

    add new component

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -2,6 +2,7 @@ import Link from "next/link";
 import NavStyles from "./styles/NavStyles";
 import { Mutation } from "react-apollo";
 import { TOGGLE_CART_MUTATION } from "./Cart";
+import CartCounter from "./CartCounter";
 
 const Nav = () => (
   <NavStyles>
@@ -21,7 +22,12 @@ const Nav = () => (
       <a>Account</a>
     </Link>
     <Mutation mutation={TOGGLE_CART_MUTATION}>
-      {toggleCart => <button onClick={toggleCart}>My Cart</button>}
+      {toggleCart => (
+        <div>
+          <button onClick={toggleCart}>My Cart</button>
+          <CartCounter />
+        </div>
+      )}
     </Mutation>
   </NavStyles>
 );
