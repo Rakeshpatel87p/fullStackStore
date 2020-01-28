49238c9f260171076db43a0b53efc2bdcb5cd761
rakeshpatel87p <rakesh.patel@accesscfa.com>
2020-01-28 18:41:30 -0500

    search component created

---
+++ sick-fits/frontend/components/Search.js
@@ -0,0 +1,73 @@
+import React from "react";
+import Downshift from "downshift";
+import Router from "next/router";
+import { ApolloConsumer } from "react-apollo";
+import gql from "graphql-tag";
+import debounce from "lodash.debounce";
+import { DropDown, DropDownItem, SearchStyles } from "./styles/DropDown";
+
+const SEARCH_ITEMS_QUERY = gql`
+  query SEARCH_ITEMS_QUERY($searchTerm: String!) {
+    items(
+      where: {
+        OR: [
+          { title_contains: $searchTerm }
+          { description_contains: $searchTerm }
+        ]
+      }
+    ) {
+      id
+      image
+      title
+    }
+  }
+`;
+
+class Search extends React.Component {
+  state = {
+    items: [],
+    loading: false
+  };
+  onChangeEvent = async (e, client) => {
+    console.log("Input being entered");
+    const res = await client.query({
+      query: SEARCH_ITEMS_QUERY,
+      variables: { searchTerm: e.target.value }
+    });
+    this.setState({
+      items: res.data.items,
+      loading: true
+    });
+  };
+  render() {
+    return (
+      <SearchStyles>
+        <div>
+          <ApolloConsumer>
+            {client => (
+              <form action="">
+                <input
+                  type="text"
+                  onChange={e => {
+                    e.persist();
+                    this.onChangeEvent(e, client);
+                  }}
+                />
+              </form>
+            )}
+          </ApolloConsumer>
+          <DropDown>
+            {this.state.items.map(item => (
+              <DropDownItem key={item.id}>
+                <img width="50" src={item.image} alt={item.title} />
+                {item.title}
+              </DropDownItem>
+            ))}
+          </DropDown>
+        </div>
+      </SearchStyles>
+    );
+  }
+}
+
+export default Search;
