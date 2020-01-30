38cbd08b9001f446d6991691264b3e9de4ecfb16
rakeshpatel87p <rakesh.patel@accesscfa.com>
2020-01-29 20:36:04 -0500

    new stripe checkout component added

---
+++ sick-fits/frontend/components/TakeMyMoney.js
@@ -0,0 +1,43 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+import { Query } from "react-apollo";
+import calcTotalPrice from "../lib/calcTotalPrice";
+import { CART_ITEMS_QUERY } from "./Cart";
+class TakeMyMoney extends React.Component {
+  onToken = res => {
+    console.log("On Token Called!");
+    console.log(res.id);
+  };
+  render() {
+    return (
+      <Query query={CART_ITEMS_QUERY}>
+        {data => {
+          const { cartItems } = data.data;
+          return (
+            <StripeCheckout
+              amount={cartItems.reduce(
+                (tally, currentVal) =>
+                  tally + currentVal.price * currentVal.price,
+                0
+              )}
+              name="Sick Fits"
+              description={`Order of ${cartItems.reduce(
+                (tally, currentVal) =>
+                  tally + currentVal.price * currentVal.quantity,
+                0
+              )} items!`}
+              image={cartItems[0] && cartItems[0].image}
+              stripeKey="pk_test_ewDeV5z2M97iMnUBD5CnPceY00UzizBuef"
+              currency="USD"
+              token={res => this.onToken(res)}
+            >
+              {this.props.children}
+            </StripeCheckout>
+          );
+        }}
+      </Query>
+    );
+  }
+}
+
+export default TakeMyMoney;
