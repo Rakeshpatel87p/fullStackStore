638888018c0ca5ea08b8cf7742176068cd72505c
rakeshpatel87p <rakesh.patel@accesscfa.com>
2020-02-11 11:03:27 -0500

    Item tests added using snapshot

---
+++ sick-fits/frontend/__tests__/Item.test.js
@@ -0,0 +1,42 @@
+import { shallow, mount } from "enzyme";
+import toJSON from "enzyme-to-json";
+import ItemComponent from "../components/Item";
+
+const fakeItem = {
+  id: "ABC123",
+  title: "A Cool Item",
+  price: 4000,
+  description: "This item is really cool!",
+  image: "dog.jpg",
+  largeImage: "largedog.jpg"
+};
+
+describe("<Item />", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<ItemComponent item={fakeItem} />);
+  });
+  it("renders and matches the snapshot", () => {
+    expect(toJSON(wrapper)).toMatchSnapshot();
+  });
+
+  it("renders the image properly", () => {
+    const img = wrapper.find("img");
+    expect(img.props().src).toBe(fakeItem.image);
+    expect(img.props().alt).toBe(fakeItem.title);
+  });
+
+  it("renders the pricetag and title", () => {
+    const pricetag = wrapper.find("PriceTag");
+    expect(pricetag.children().text()).toBe("$40");
+  });
+
+  it("renders out the buttons properly", () => {
+    const wrapper = shallow(<ItemComponent item={fakeItem} />);
+    const buttonList = wrapper.find(".buttonList");
+    expect(buttonList.children()).toHaveLength(3);
+    expect(buttonList.find("Link")).toHaveLength(1);
+    expect(buttonList.find("AddToCart").exists()).toBe(true);
+    expect(buttonList.find("DeleteItem").exists()).toBe(true);
+  });
+});
