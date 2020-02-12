ffc9e8391db8db6ae0df1862332c2a0ddbaae157
rakeshpatel87p <rakesh.patel@accesscfa.com>
2020-02-11 23:12:24 -0500

    new test

---
+++ sick-fits/frontend/__tests__/CartCount.test.js
@@ -0,0 +1,13 @@
+// import { shallow, mount } from "enzyme";
+// import toJSON from "enzyme-to-json";
+// import CartCount from "../components/CartCounter";
+
+describe("<CartCount />>", () => {
+  it("equals 2", () => {
+    expect(2).toEqual(2);
+  });
+  //   it("renders", () => {
+  //     const wrapper = mount(<CartCount count={11} />);
+  //     expect.toJSON(wrapper).toMatchSnapshot();
+  //   });
+});
